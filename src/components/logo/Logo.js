38508d195e8c8558edf4e4c2c4d880fe277ed0e5
pawel misiak <pawelmisiak@live.com>
2018-03-29 14:35:51 -0400

    finish working on Logo styling and moving on to the Image Link Form

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -6,11 +6,20 @@ import brain from './brain.png';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 35 , reverse: true }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner pa3 dc"><img style={{paddingTop: '5px'}} src={brain} alt='logo'/> </div>
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max : 35 , reverse: true }}
+        style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3 dc">
+          <img
+            style={{paddingTop: '5px', height: 110, width: 110}}
+            src={brain}
+            alt='logo'/>
+        </div>
       </Tilt>
     </div>
   );
 }
+// for div's and img in className I used tachyons styling for easier application
 
 export default Logo;
