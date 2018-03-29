ff0f1146cd56fa7190a4a31596369c54dc208ec4
pawel misiak <pawelmisiak@live.com>
2018-03-29 16:51:09 -0400

    finish work on Rank. Also added moving particle background to make the website more dynamic

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }}
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles params={particlesOptions} className='particles'/>
         <Navigation />
         <Logo />
-        <ImageLinkForm />
         <Rank />
+        <ImageLinkForm />
         {/* <FaceRecognition/> */}
       </div>
     );
