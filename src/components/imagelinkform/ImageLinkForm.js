56f6fea444632fa6d61fb885af73d38433d68356
pawel misiak <pawelmisiak@live.com>
2018-03-29 16:19:38 -0400

    finish working on Image Link Form Styling

--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
   return (
     <div>
-      <p>
+      <p className='f3'>{/* tachyon styling */}
         {'Insert A picture and I will find a face in it.'}
       </p>
+      <div className='center'>
+        <div className='pa4 br3 shadow-5 form center'>{/* tachyon styling */}
+          <input className='f4 pa2 w-70 center' type='text'/> {/* tachyon styling */}
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-gold'>
+            Detect
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
