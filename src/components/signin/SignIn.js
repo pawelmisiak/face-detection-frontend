88ace0f0d5b135a2b41be2d480bd0b9b0f40aaae
pawel misiak <pawelmisiak@live.com>
2018-04-03 09:49:53 -0400

    finish work on SignIn module, it is able to receive a email and password and compare it to the object temporarily stored in local variable. Now moving on to work on Register module

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -8,7 +8,7 @@
      super(props);
      this.state = {
        signInEmail: '',
-       signInPassword: '',
+       signInPassword: ''
      }
    }
 
@@ -21,12 +21,24 @@
    }
 
    onSubmitSignIn = () => {
-     console.log(this.state.signIn);
+     fetch('http://localhost:3000/signin', {
+       method: 'post',
+       headers: {'Content-Type': 'application/json'},
+       body: JSON.stringify({
+         email: this.state.signInEmail,
+         password: this.state.signInPassword
+       })
+     })
+      .then(response => response.json())
+      .then(data => {
+        if (data === 'success') {
+          this.props.onRouteChange('home');
+        }
+      })
    }
 
    render() {
     const { onRouteChange } = this.props;
-
      return (
        <article className="br3 ba b--black-10 mv4 mw6 shadow-5 center">
          <main className="pa4 black-80">
@@ -35,16 +47,28 @@
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={() => onRouteChange('home')} //calling the function on click
+                onClick={ this.onSubmitSignIn } //calling the function on click
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Sign in" />
