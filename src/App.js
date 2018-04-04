f7c29fd72f8f8a8f32b34105d6960903996e27bb
pawel misiak <pawelmisiak@live.com>
2018-04-04 18:25:12 -0400

    fix bug that will make information carry over from one profile to another without clearing the state of user

--- src/App.js
+++ src/App.js
@@ -32,23 +32,26 @@ const particlesOptions = { //options for the dynamic bg
       }}
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id:'',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id:'',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
+
   }
 
   loadUser = (data) => {
@@ -116,7 +119,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
